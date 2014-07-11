17d3ed6481df6ab435c4ae8c5e6c20c6cf268bb7
thebvog <bvog@yandex.ru>
2014-07-12 00:06:16 +0400

    Standings and contest color option

--- README.md
+++ README.md
@@ -7,13 +7,19 @@ Run:
   conky.py [options]
 
 Parameters:
+
   visual options:
-  --hr      - output $hr after every block
-  --colors  - make output more pretty, add ${color ...}, e.g. to change user rank
+
+  --hr              - output $hr after every block
+  --colors          - make output more pretty, add ${color ...}, e.g. to change user rank
+  --contest-color   - set up contest color, in next order BEFORE, RUNNING, PENDING_SYSTEM_TEST, SYSTEM_TEST, FINISHED.
+  If you need skip some color, don`t change, just don`t write color, e.g #ffffff,,#ffffff,#ffa200
 
   blocks:
-  --next-contest-list           - show list of future contests with date
-  --user-info handle,...,handle - show user info for user in list
+
+  --next-contest-list                   - show list of future contests with date
+  --user-info handle,...,handle         - show user info for user in list
+  --current-standings handle,...,handle - show list of current standings for user in list
   
   Sample .conky line (updated every 16 seconds):
 ${execpi 16  python ~/source/cfclass/conky.py --next-contest-list --hr --colors}
--- conky.py
+++ conky.py
@@ -2,6 +2,7 @@ import cfclass
 import sys
 import os
 import datetime
+import re
 
 
 
@@ -12,6 +13,12 @@ def to_int(name, value):
         print(name + ' = ' + str(value) + ' must be integer')
         sys.exit(0)
 
+
+def is_color(value):
+    p = re.compile(r'[0-9a-f]{6}')
+    return p.match(value)
+
+
 # main object of CodeForces API
 cf = cfclass.CodeForces()
 
@@ -19,34 +26,84 @@ cf = cfclass.CodeForces()
 top = -1
 hr = False
 colors = False
+division = 'all'
+handles = []
 special_chars = [
     {'from': '$', 'to': '\$'},
     {'from': '#', 'to': '\#'},
     {'from': '\n', 'to': ' '},
     {'from': '\r', 'to': ' '}
 ]
+contest_color_names = [
+    'BEFORE',
+    'RUNNING',
+    'PENDING_SYSTEM_TEST',
+    'SYSTEM_TEST',
+    'FINISHED'
+]
 contest_colors = {
-    'BEFORE': '${color #ff7373}',
+    'BEFORE': '${color #ffa200}',
     'RUNNING': '${color #ffee59}',
     'PENDING_SYSTEM_TEST': '${color #ffee59}',
     'SYSTEM_TEST': '${color #ffee59}',
-    'FINISHED': '${color #6fff6e}'
+    'FINISHED': '${color #daff5c}'
+}
+submission_colors = {
+    'AC': '${color 02af1b}',
+    'WA': '${color a9100f}',
+    'NO': '${color}'
+
 }
 argc = len(sys.argv)
 for i in range(1, argc):
-    if (sys.argv[i] == '--top') and (i < argc - 1):
-        top = to_int('--top', sys.argv[i + 1])
-    if sys.argv[i] == '--hr':
+    param = sys.argv[i]
+    value = None
+    if i < argc - 1:
+        value = sys.argv[i + 1]
+
+    if param == '--top' and value:
+        top = to_int('--top', value)
+
+    if param == '--hr':
         hr = True
-    if sys.argv[i] == '--colors':
+
+    if param == '--colors':
         colors = True
+
+    if param == '--div1':
+        division = 'div1'
+    if param == '--div2':
+        division = 'div2'
+
+    if param == '--handles':
+        handles = value.split(',')
+
+    if param == '--contest-colors':
+        if not value:
+            continue
+        value_list = value.split(',')
+        for i in range(0, min(len(contest_color_names), len(value_list))):
+            if not value_list[i] or not is_color(value_list[i]):
+                continue
+            contest_colors[contest_color_names[i]] = '${color #%s}' % (value_list[i])
+
 
 # select main response function
 for i in range(1, len(sys.argv)):
-    if sys.argv[i] == '--next-contest-list':
+    param = sys.argv[i]
+    value = None
+    if i < argc - 1:
+        value = sys.argv[i + 1]
+
+    if param == '--next-contest-list':
         result = cf.getContestList()
         for contest in result:
             if contest['phase'] != 'BEFORE':
+                continue
+
+            if division == 'div1' and contest['name'].find('Div. 2') != -1:
+                continue
+            if division == 'div2' and contest['name'].find('Div. 1') != -1:
                 continue
 
             # filter special characters
@@ -69,8 +126,36 @@ for i in range(1, len(sys.argv)):
         if hr:
             print('$color$hr')
 
-    if (sys.argv[i] == '--user-info') and (i < argc - 1):
-        handles = sys.argv[i + 1]
+    if param == '--contest-list':
+        result = cf.getContestList(top=top)
+        for contest in result:
+            if division == 'div1' and contest['name'].find('Div. 2') != -1:
+                continue
+            if division == 'div2' and contest['name'].find('Div. 1') != -1:
+                continue
+
+            # filter special characters
+            for char in special_chars:
+                contest['name'] = contest['name'].replace(char['from'], char['to'])
+
+            # add time
+            if colors:
+                line = '%d %s%s $color' % (contest['id'], contest_colors[contest['phase']], contest['name'])
+            else:
+                line = '%d %s' % (contest['id'], contest['name'])
+            date = datetime.datetime.fromtimestamp(
+                    int(contest['startTimeSeconds'])
+            ).strftime('%d.%m.%Y %H:%M:%S')
+            line += os.linesep + '\t' + date
+
+            # output
+            print(line)
+
+        if hr:
+            print('$color$hr')
+
+    if param == '--user-info' and value:
+        handles = value
         handles = handles.split(',')
         result = cf.getUserInfo(handles)
         for user in result:
@@ -83,7 +168,91 @@ for i in range(1, len(sys.argv)):
         if hr:
             print('$color$hr')
 
+    if param == '--current-standings':
+        result = cf.getContestList(top=4)
+        if not handles:
+            continue
+        for contest in result:
+            if contest['phase'] != 'RUNNING':
+                continue
+
+            if division == 'div1' and contest['name'].find('Div. 2') != -1:
+                continue
+            if division == 'div2' and contest['name'].find('Div. 1') != -1:
+                continue
+
+            # filter special characters
+            for char in special_chars:
+                contest['name'] = contest['name'].replace(char['from'], char['to'])
+
+            standings = cf.getContestStandings(contestId=contest['id'], handles=handles)
+            if not standings:
+                continue
+            if len(standings['rows']) == 0:
+                continue
+
+            # show title of contest
+            if colors:
+                line = '%d %s%s $color' % (contest['id'], contest_colors[contest['phase']], contest['name'])
+            else:
+                line = '%d %s' % (contest['id'], contest['name'])
+            print(line)
+
+            # calculate max handle len for align
+            user_len = 0
+            for row in standings['rows']:
+                prefix = '%d %s' % (row['rank'], row['party']['members'][0]['handle'])
+                user_len = max(user_len, len(prefix))
+            user_len += 1
+
+            # problems list
+            line = ' ' * user_len
+            for problem in standings['problems']:
+                line += ' %s ' % (problem['index'])
+            print(line)
+
+            # lines with handles
+            for row in standings['rows']:
+                prefix = '%d %s' % (row['rank'], row['party']['members'][0]['handle'])
+                line = prefix + ' ' * (user_len - len(prefix))
+                if colors:
+                    line = '${color}' + line
+                for cell in row['problemResults']:
+                    # cell content with sign & attempts
+                    cell_str = ''
+                    # color len in text for align
+                    cell_color_len = 0
+                    # if accepted
+                    if cell['points'] == 0.0:
+                        # show sign if attempts > 1
+                        if cell['rejectedAttemptCount'] > 0:
+                            if colors:
+                                cell_str += submission_colors['WA']
+                                cell_color_len = len(submission_colors['WA'])
+                            cell_str += '-' + str(cell['rejectedAttemptCount'])
+                        else:
+                            if colors:
+                                cell_str += submission_colors['NO']
+                                cell_color_len = len(submission_colors['NO'])
+                            cell_str += ' .'
+                    # wa or no submissions
+                    else:
+                        if colors:
+                            cell_str += submission_colors['AC']
+                            cell_color_len = len(submission_colors['AC'])
+                        # show sign if attempts > 1
+                        if cell['rejectedAttemptCount'] > 0:
+                            cell_str += '+' + str(cell['rejectedAttemptCount'])
+                        else:
+                            cell_str += ' +'
+                    cell_str += (cell_color_len + 3 - len(cell_str)) * ' '
+                    line += cell_str
+                print(line)
+
+            if hr:
+                print('$color$hr')
+
     # debug default method
-    if sys.argv[i] == '--debug':
+    if param == '--debug':
         result = cf.getContestStandings(374)
         print(result)
